3ed1f96b31f772cf1031f4dac2d2b467063d77b4
usiyoung <usiyoung7@gmail.com>
2025-09-02 18:33:21 +0900

    feat: test

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,19 +1,19 @@
 "use client";
 
 import { type ReactNode } from "react";
-import { base } from "wagmi/chains";
+import { baseSepolia } from "wagmi/chains";
 import { MiniKitProvider } from "@coinbase/onchainkit/minikit";
 
 export function Providers(props: { children: ReactNode }) {
   return (
     <MiniKitProvider
       apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
-      chain={base}
+      chain={baseSepolia}
       config={{
         appearance: {
           mode: "auto",
           theme: "mini-app-theme",
-          name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
+          name: 'SOOHO',
           logo: process.env.NEXT_PUBLIC_ICON_URL,
         },
       }}
