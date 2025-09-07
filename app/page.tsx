138662ee70e46bfff3a347ee4d90d60ee8bb1eb3
usiyoung <usiyoung7@gmail.com>
2025-09-07 17:52:17 +0900

    feat: eruda

--- app/page.tsx
+++ app/page.tsx
@@ -34,9 +34,7 @@ export default function App() {
 
   useEffect(() => {
     // 개발 환경이고, localhost가 아닐 때만 Eruda를 로드
-    if (process.env.NODE_ENV === 'development' && !window.location.hostname.includes('localhost')) {
       import('eruda').then((eruda) => eruda.default.init());
-    }
   }, []);
 
   useEffect(() => {
