1200bf8e1c4c2c4cf5798aab3be57158977be509
usiyoung <usiyoung7@gmail.com>
2025-09-07 18:08:45 +0900

    fix: meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,6 +52,29 @@ export default function RootLayout({
   return (
     <html lang="en">
    
+   <head>
+   <link rel="shortcut icon" type="image/png" href="/aurora_pay.png" />
+    <link rel="apple-touch-icon" sizes="192x192" href="/aurora_pay.png" />
+    <link rel="apple-touch-icon" sizes="512x512" href="/aurora_pay.png" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta name="theme-color" content="#fff" />
+    <meta name="format-detection" content="telephone=no" />
+    <meta name="description" content="Swap crypto on Ethereum, Base, Arbitrum, Polygon, Unichain and more. The DeFi platform trusted by millions." data-rh="true" />
+    <meta property="og:title" content="Aurora Pay" data-rh="true" />
+    <meta property="og:description" content="Swap crypto on Ethereum, Base, Arbitrum, Polygon, Unichain and more. The DeFi platform trusted by millions." data-rh="true"/>
+    <meta property="og:image" content="/aurora_pay.png" data-rh="true"/>
+    <meta property="og:image:width" content="1200" data-rh="true"/>  
+    <meta property="og:image:height" content="630" data-rh="true"/>
+    <meta property="og:image:alt" content="Aurora Pay" data-rh="true"/>
+    <meta property="og:type" content="website" data-rh="true"/>
+    <meta property="og:url" content="https://aurora-mini-app.netlify.app/.well-known/farcaster.json" data-rh="true"/>
+    <meta property="twitter:card" content="summary_large_image" data-rh="true"/>
+    <meta property="twitter:title" content="Aurora Pay" data-rh="true"/>
+    <meta property="twitter:image" content="/aurora_pay.png" data-rh="true"/>
+    <meta property="twitter:image:alt" content="Aurora Pay" data-rh="true"/>
+
+   </head>
     <body className="bg-background">
       <Providers>{children}</Providers>
     </body>
