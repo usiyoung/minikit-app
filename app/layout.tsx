a4743eb8295b14763c12b7c83b32912b1f4fd1bd
usiyoung <usiyoung7@gmail.com>
2025-09-07 17:56:52 +0900

    fix: meat

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const viewport: Viewport = {
 };
 
 export async function generateMetadata(): Promise<Metadata> {
-  // const URL = process.env.NEXT_PUBLIC_URL;
+  const URL = process.env.NEXT_PUBLIC_URL;
   return {
     title: "Aurora Pay", 
     description: "Aurora Pay leads the evolution of digital payments. We provide a simplified and frictionless solution for stablecoin-to-fiat conversion.",
@@ -24,23 +24,23 @@ export async function generateMetadata(): Promise<Metadata> {
         url: '/aurora_pay.png',
       },
     },
-    // other: {
-    //   "fc:frame": JSON.stringify({
-    //     version: "next",
-    //     imageUrl: '/aurora_pay.png',
-    //     button: {
-    //       title: `Launch ${process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME}`,
-    //       action: {
-    //         type: "launch_frame",
-    //         name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
-    //         url: URL,
-    //         splashImageUrl: process.env.NEXT_PUBLIC_SPLASH_IMAGE,
-    //         splashBackgroundColor:
-    //           process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR,
-    //       },
-    //     },
-    //   }),
-    // },
+    other: {
+      "fc:frame": JSON.stringify({
+        version: "next",
+        imageUrl: '/aurora_pay.png',
+        button: {
+          title: `Launch ${process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME}`,
+          action: {
+            type: "launch_frame",
+            name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
+            url: URL,
+            splashImageUrl: process.env.NEXT_PUBLIC_SPLASH_IMAGE,
+            splashBackgroundColor:
+              process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR,
+          },
+        },
+      }),
+    },
   };
 }
 
